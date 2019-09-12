63723c2536a05f5337bb90dedfd52f1bb4250403
Craig Jolley <jolleycraig@gmail.com>
2019-09-12 15:07:18 -0400

    Factored out repeated code between scenario_compare() and country_compare()

--- IFs_plots.R
+++ IFs_plots.R
@@ -1,10 +1,4 @@
-library(dplyr)
-library(ggplot2)
-library(forcats)
-library(reshape2)
-library(readr)
-
-# TODO: just load tidyverse, instead of all the individual pieces
+library(tidyverse)
 
 ###############################################################################
 # Define USAID themes & colors
@@ -28,11 +22,11 @@ theme_USAID <- theme(
 
 approved_colors <-c(
   '#002F6C', # USAID blue
-  '#BA0C2F',  # USAID red
-  '#6C6463',   # dark gray
+  '#BA0C2F', # USAID red
+  '#6C6463', # dark gray
   '#A7C6ED', # light blue
   '#651D32', # dark red
-  '#CFCDC9' # light gray
+  '#CFCDC9'  # light gray
 ) 
 
 colors_USAID <- scale_color_manual(values=approved_colors)
@@ -50,10 +44,9 @@ hf_line <- function(label_height,cutoff=2014.5,split=0.25,text_orient='horizonta
     list(line,
          annotate('text',y=label_height,x=cutoff,hjust=1+split,label='History'),
          annotate('text',y=label_height,x=cutoff,hjust=-split,label='Forecast'))
-  }
-  if (text_orient == 'vertical') {
+  } else if (text_orient == 'vertical') {
     list(line,
-         annotate('text',y=label_height,x=cutoff,vjust=-split,angle=90,label='History') +
+         annotate('text',y=label_height,x=cutoff,vjust=-split,angle=90,label='History'),
          annotate('text',y=label_height,x=cutoff,vjust=1+split,angle=90,label='Forecast'))
   }
 }
@@ -106,6 +99,20 @@ y_percent <- function(accuracy=1) {
   scale_y_continuous(labels=scales::percent_format(accuracy=accuracy))
 }
 
+### Shared features across plots
+### TODO: more elegant way to pass standard set of argument with ellipsis?
+line_plot <- function(valpercent=FALSE,dots=FALSE,ontop=NULL) {
+  out <- list(geom_line(size=1),
+              xlab('Year'),
+              theme_USAID,
+              colors_USAID)
+  if (valpercent) { 
+    x <- append(x,scale_y_continuous(labels=scales::percent_format(accuracy=1))) 
+  }
+  if (dots) { out <- append(out,geom_point(size=2)) }
+  out
+}
+
 
 ###############################################################################
 # Rename countries and groups
@@ -123,35 +130,34 @@ geo_rename <- function(x) {
 
 ###############################################################################
 # Country-comparison plot
+# TODO: factor out a single function to handle shared features
 ###############################################################################
-country_compare <- function(fname,ytitle,dots=TRUE,label_split=0.2, 
-                            label_height=0.9, ontop=NULL, 
-                            text_orient='horizontal') {
+country_compare <- function(fname,ytitle,cutoff=2014.5,label_split=0.2,
+                            label_height=0.9,text_orient='horizontal',
+                            valpercent=FALSE,dots=FALSE,ontop=NULL) {
   ifs_df <- read_tsv(fname, col_names = c('varstr','year','value')) %>%
     na.omit %>%
-    mutate(Country = sub('^.*\\(','',varstr) %>% sub(',.*?\\)','',.) %>% sub('\\)'2,'',.),
+    mutate(Country = sub('^.*\\(','',varstr) %>% sub(',.*?\\)','',.) %>% sub('\\)','',.),
            Country = geo_rename(Country),
            Country = fct_reorder(Country,value,.desc=TRUE)) %>%
     select(Country,year,value)
+  if (valpercent) { ifs_df <- mutate(ifs_df,value=value/100)}
   
   p <- ggplot(data=ifs_df,aes(x=year,y=value,group=Country,color=Country)) +
-    geom_line(size=1) +
-    xlab('Year') +
-    ylab(ytitle) +
-    theme_USAID + colors_USAID
+    line_plot(valpercent,dots,ontop) +
+    ylab(ytitle) 
   if (max(ifs_df$year) > cutoff & min(ifs_df$year) < cutoff) {
     lh <- min(ifs_df$value) + label_height*(max(ifs_df$value) - min(ifs_df$value))
     p <- p + hf_line(lh,split=label_split,text_orient=text_orient)
   }
-  if (dots) { p <- p + geom_point(size=2)}
   if (!is.null(ontop)) {
     p <- p + geom_line(data=ifs_df[ifs_df$Country==ontop,],size=1)
   }
   p
 }
 
-## TODO: make sure that country_compare and scenario_compare have matching
-# argument lists and follow a parallel structure to the extent possible.
+## TEST CODE
+#country_compare('../Kenya/IFs_exports/life_exp.txt','Life expectancy (years)')
 
 ###############################################################################
 # Scenario-comparison plot
@@ -159,9 +165,10 @@ country_compare <- function(fname,ytitle,dots=TRUE,label_split=0.2,
 # able to pass in a "recode" data frame and join it to ifs_df to rename the
 # scenarios. Also get rid of base_label argument.
 ###############################################################################
-scenario_compare <- function(fname,ytitle,cutoff=2014.5,text_orient='horizontal',
-                             base_label='Base',valpercent=FALSE,label_height=0.9,dots=FALSE,
-                             ontop=NULL) {
+scenario_compare <- function(fname,ytitle,cutoff=2014.5,label_split=0.2,
+                             label_height=0.9,text_orient='horizontal',
+                             valpercent=FALSE,dots=FALSE,ontop=NULL,
+                             base_label='Base') {
   ifs_df <- read_tsv(fname, col_names = c('varstr','year','value')) %>%
     na.omit %>%
     mutate(Scenario = sub('^.*\\[','',varstr) %>% sub('\\].*','',.),
@@ -173,30 +180,29 @@ scenario_compare <- function(fname,ytitle,cutoff=2014.5,text_orient='horizontal'
            Scenario = fct_reorder(Scenario,value,.desc=TRUE)) %>%
     select(Scenario,year,value)
   if (valpercent) { ifs_df <- mutate(ifs_df,value=value/100)}
-  label_height <- min(ifs_df$value) + lh*(max(ifs_df$value) - min(ifs_df$value))
+
   p <- ggplot(data=ifs_df,aes(x=year,y=value,group=Scenario,color=Scenario)) +
-    geom_line(size=1) +
-    xlab('Year') +
-    ylab(ytitle) +
-    theme_USAID + colors_USAID 
-  if (valpercent) { p <- p + scale_y_continuous(labels=scales::percent_format(accuracy=1)) }
-  if (max(ifs_df$year) > cutoff & min(ifs_df$year) < cutoff) { 
+    line_plot(valpercent,dots,ontop) +
+    ylab(ytitle) 
+  if (max(ifs_df$year) > cutoff & min(ifs_df$year) < cutoff) {
     lh <- min(ifs_df$value) + label_height*(max(ifs_df$value) - min(ifs_df$value))
     p <- p + hf_line(lh,split=label_split,text_orient=text_orient)
   }
-  if (dots) { p <- p + geom_point(size=2)}
   if (!is.null(ontop)) {
     p <- p + geom_line(data=ifs_df[ifs_df$Scenario==ontop,],size=1)
-    if (dots) { p <- p + geom_point(data=ifs_df[ifs_df$Scenario==ontop,],size=2)}
   }
   p
 }
 
+### TEST CODE
+#scenario_compare('../Ghana/IFs_exports/final2-aid.txt','Billion dollars')
+
 ## TODO: what other standardized plot types would it be good to have?
 
-# comparison of multiple countries across multiple scenarios, encoding 
+################################################################################
+# Comparison of multiple countries across multiple scenarios, encoding 
 # countries with colors and scenarios with line-types (or vice versa)
-# (example from report?)
+################################################################################
 
 # cross-sectional scatterplot, drawing from two different IFs export files and
 # with a specific country (or set of countries) highlighted
